6b630111a46a32857c876dbb4e0c4e1a10fab28f
Tyler Madsen <tylermadsen@mail.weber.edu>
2017-03-30 15:36:31 +0000

    initial uploads

---
+++ Orion_Mod9_hw8_task1.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Orion_Mod9_hw8_task1.c
+ *       	Usage:  ./Orion_Mod9_hw8_task1.c
+ *    Description:  2 strings for input parameters
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:18:16 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tyler Madsen (), tylermadsen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+#include <time.h>
+
+/* Function Prototypes */
+
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
---
+++ Orion_Mod9_hw8_task2.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Orion_Mod9_hw8_task2.c
+ *       	Usage:  ./Orion_Mod9_hw8_task2.c
+ *    Description:  Input to End-of-File
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2017 04:18:40 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tyler Madsen (), tylermadsen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+#include <time.h>
+
+/* Function Prototypes */
+
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
